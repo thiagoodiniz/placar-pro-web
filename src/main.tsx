f8b93d31653f819d2bd0659d3d5a874e8f3dadb6
Thiago <you@example.com>
2026-04-23 22:25:59 -0300

    feat: implement sentry

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ConfigProvider } from 'antd'
 import ptBR from 'antd/locale/pt_BR'
+import * as Sentry from "@sentry/react"
 import App from './App.tsx'
 import { themeConfig } from './theme/theme.ts'
 import './index.css'
 import { initAnalytics } from './services/analytics.ts'
+
+// Inicializa o Sentry
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    sendDefaultPii: true
+});
 
 // Inicializa o PostHog Analytics
 initAnalytics();
@@ -23,7 +37,9 @@ if (storedVersion !== APP_VERSION) {
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ConfigProvider theme={themeConfig} locale={ptBR}>
-        <App />
-    </ConfigProvider>,
+    <Sentry.ErrorBoundary fallback={<div>Ocorreu um erro. Nossa equipe foi notificada.</div>}>
+        <ConfigProvider theme={themeConfig} locale={ptBR}>
+            <App />
+        </ConfigProvider>
+    </Sentry.ErrorBoundary>,
 )
